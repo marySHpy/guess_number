9e6c6dada97ec07c9c41e754cddd70321d13a73c
Мария Шарко <mariasharkomgimo@gmail.com>
2026-01-16 11:33:38 +0300

    Add initial commit

---
+++ guess_number.py
@@ -0,0 +1,24 @@
+from random import randint
+
+number = randint(1, 100)
+welcome_text = 'Угадайте число от 1 до 100'
+
+#gegege
+
+def main():
+    while True:
+        guess = int(input('Введите число: '))
+
+        if guess < number:
+            print('Ваше число меньше того, что загадано.')
+
+        if guess > number:
+            print('Ваше число больше того, что загадано.')
+
+        if guess == number:
+            break
+
+main()
+
+
+print('Отличная интуиция! Вы угадали число :)')
